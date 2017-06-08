3ff4323621d04e213c8d7e02cb67efcf73ff842f
Micah Isley <micahisley@Micahs-MacBook-Air.local>
2017-06-08 19:17:55 -0400

    more homework

--- liri.js
+++ liri.js
@@ -8,37 +8,36 @@ var client = new Twitter({
 });
 
 var spotifyObject;
-//var params = {q: 'MisleyTest'};
-//client.get('search/tweets', params, function(error, tweets, response) {
- // if (!error) {
-  //	console.log(tweets.statuses);
- // }
-//});
+var params = {q: 'MisleyTest'};
+client.get('search/tweets', params, function(error, tweets, response) {
+  if (!error) {
+  	console.log(tweets.statuses[1].text);
+  }
+});
 
 //var Spotify = require('node-spotify-api');
  
 //var spotify = new Spotify({
 //  id: "f29652ed86a3463798b93acdd075bcb7",
-//  secret: "e6ebd63ab0fe4b5486b314544692266b"
+ // secret: "e6ebd63ab0fe4b5486b314544692266b"
 //});
  
 //spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  //if (err) {
-    //return console.log('Error occurred: ' + err);
-  //}
- 
-  //spotifyObject = data.tracks.items; 
-  //console.log(spotifyObject.album.artists);
+//  if (err) {
+//    return console.log('Error occurred: ' + err);
+ // }
+ // spotifyObject = (data); 
+//  console.log(spotifyObject.tracks.items[1].album.name);
 //});
-var movieObject;
+//var movieObject;
 
-var request = require('request');
-request('http://www.omdbapi.com/?apikey=40e9cece&t=lord+of+the+rings', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred 
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received // Print the HTML for the Google homepage. 
-  movieObject = (JSON.stringify(body));
-  console.log(movieObject.results.Title);
-});
+//var request = require('request');
+//request('http://www.omdbapi.com/?apikey=40e9cece&t=lord+of+the+rings', function (error, response, body) {
+ // console.log('error:', error); // Print the error if one occurred 
+ // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received /
+ // movieObject = (JSON.parse(body));
+ // console.log(movieObject.Title);
+//});
 
 
  
